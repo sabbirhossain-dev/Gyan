2150c7821e86c9c2c7f400fdd58456b6cf5ab389
sabbirhossain-dev <hossainsabbir309@gmail.com>
2020-11-25 12:15:45 +0600

    add meta tag

--- js/script.js
+++ js/script.js
@@ -51,7 +51,8 @@ $('#banner-part').slick({
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: true,
+
             }
         },
         {
@@ -64,6 +65,7 @@ $('#banner-part').slick({
         {
             breakpoint: 576,
             settings: {
+                autoPlay: true,
                 dots: true,
                 arrows: false,
                 slidesToShow: 1,
@@ -169,7 +171,7 @@ $('.courses-slider').slick({
             breakpoint: 576,
             settings: {
                 arrows: false,
-                dots:true,
+                dots: true,
                 slidesToShow: 1,
                 slidesToScroll: 1
             }
@@ -196,29 +198,29 @@ $('.leftt').slick({
     slidesToScroll: 1,
     prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
     nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
-      responsive: [
+    responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
         },
         {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
         }
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
@@ -246,30 +248,30 @@ $('.footer-slider').slick({
     centerPadding: 0,
     arrows: false,
 
-      responsive: [
+    responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
         },
         {
-          breakpoint: 576,
-          settings: {
-              autoPlay:true,
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
+            breakpoint: 576,
+            settings: {
+                autoPlay: true,
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
         }
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
